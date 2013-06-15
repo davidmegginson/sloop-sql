c10c7b76b413ac869001d6979a7c25b3768ac21c
David Megginson <david.megginson@gmail.com>
2013-06-15 16:23:56 -0400

    Simple widget functionality working.

--- src/com/megginson/sloopsql/MainActivity.java
+++ src/com/megginson/sloopsql/MainActivity.java
@@ -3,6 +3,8 @@ package com.megginson.sloopsql;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends Activity
 {
@@ -13,4 +15,16 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+	/**
+	  Event: execute a SQL query.
+	 */
+	public void doExecuteQuery(View view)
+	{
+		EditText queryView = (EditText)findViewById(R.id.input_query);
+		String queryText = queryView.getText().toString();
+
+		TextView resultsView = (TextView)findViewById(R.id.text_results);
+		resultsView.setText(queryText);
+	}
 }
