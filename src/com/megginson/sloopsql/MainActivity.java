c55d5e1b75e2395d0f8861804b4c6737ce6e2642
David Megginson <david.megginson@gmail.com>
2013-06-29 11:35:03 -0400

    Working with three hard-coded tabs

--- src/com/megginson/sloopsql/MainActivity.java
+++ src/com/megginson/sloopsql/MainActivity.java
@@ -1,12 +1,17 @@
 package com.megginson.sloopsql;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import com.megginson.sloopsql.R;
-import android.widget.Toast;
 
+/**
+ * Main container activity for the UI.
+ *
+ * This activity manages the various fragments that make up the app.
+ */
 public class MainActivity extends Activity
 {
 
@@ -15,6 +20,19 @@ public class MainActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		final ActionBar actionBar = getActionBar();
+
+		// Specify that tabs should be displayed in the action bar.
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		// Add 3 tabs, specifying the tab's text and TabListener
+		for (int i = 0; i < 3; i++) {
+			actionBar.addTab(
+                actionBar.newTab()
+				.setText("Tab " + (i + 1))
+				.setTabListener(new TabListener<QueryFragment>(this, "query" + i, QueryFragment.class)));
+		}
 	}
 
 }
